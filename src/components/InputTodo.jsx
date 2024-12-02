ee1d39dc6c2abbd7642cf46f271c014993cc5bb9
Saidi Qemali <sajdiqemali@gmail.com>
2024-12-02 16:43:35 +0100

    Input ToDo erweitert mit Prio, Kategorie und Fälligkeitsdatum Felder

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,10 +1,12 @@
-/* eslint react/prop-types: 0 */
 import { useState } from "react";
 import { FaPlusCircle } from "react-icons/fa";
 
 const InputTodo = (props) => {
   const [inputText, setInputText] = useState({
     title: "",
+    priority: "Medium", // Default value
+    category: "",
+    dueDate: "",
   });
 
   const onChange = (e) => {
@@ -17,9 +19,12 @@ const InputTodo = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (inputText.title.trim()) {
-      props.addTodoProps(inputText.title);
+      props.addTodoProps(inputText.title, inputText.priority, inputText.category, inputText.dueDate);
       setInputText({
         title: "",
+        priority: "Medium",
+        category: "",
+        dueDate: "",
       });
     } else {
       alert("Please write item");
@@ -27,23 +32,48 @@ const InputTodo = (props) => {
   };
 
   return (
-    <form
-      data-set="todo-form"
-      onSubmit={handleSubmit}
-      className="form-container"
-    >
-      <input
-        type="text"
-        className="input-text"
-        placeholder="Add todo..."
-        value={inputText.title}
-        name="title"
-        onChange={onChange}
-      />
-      <button data-set="add-todo-btn" className="input-submit">
-        <FaPlusCircle />
-      </button>
-    </form>
+      <form
+          data-set="todo-form"
+          onSubmit={handleSubmit}
+          className="form-container"
+      >
+        <input
+            type="text"
+            className="input-text"
+            placeholder="Add todo..."
+            value={inputText.title}
+            name="title"
+            onChange={onChange}
+        />
+        <select
+            name="priority"
+            value={inputText.priority}
+            onChange={onChange}
+            className="input-text"
+        >
+          <option value="High">High</option>
+          <option value="Medium">Medium</option>
+          <option value="Low">Low</option>
+        </select>
+        <input
+            type="text"
+            className="input-text"
+            placeholder="Category"
+            value={inputText.category}
+            name="category"
+            onChange={onChange}
+        />
+        <input
+            type="date"
+            className="input-text"
+            value={inputText.dueDate}
+            name="dueDate"
+            onChange={onChange}
+        />
+        <button data-set="add-todo-btn" className="input-submit">
+          <FaPlusCircle />
+        </button>
+      </form>
   );
 };
 
--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -6,72 +6,72 @@ import TodosList from "./TodosList";
 import styles from "./TodoContainer.module.css";
 
 const TodoContainer = () => {
-  const getInitialTodos = () => {
-    // getting stored items
-    const temp = localStorage.getItem("todos");
-    const savedTodos = JSON.parse(temp);
-    return savedTodos || [];
-  };
-
-  const [todos, setTodos] = useState(getInitialTodos());
-
-  const handleChange = (id) => {
-    setTodos((prevState) =>
-      prevState.map((todo) => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            completed: !todo.completed,
-          };
-        }
-        return todo;
-      })
-    );
-  };
-
-  const delTodo = (id) => {
-    setTodos([...todos.filter((todo) => todo.id !== id)]);
-  };
-
-  const addTodoItem = (title) => {
-    const newTodo = {
-      id: uuidv4(),
-      title,
-      completed: false,
+    const getInitialTodos = () => {
+        const temp = localStorage.getItem("todos");
+        const savedTodos = JSON.parse(temp);
+        return savedTodos || [];
     };
-    setTodos([...todos, newTodo]);
-  };
 
-  const setUpdate = (updatedTitle, id) => {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id === id) {
-          todo.title = updatedTitle;
-        }
-        return todo;
-      })
+    const [todos, setTodos] = useState(getInitialTodos());
+
+    const handleChange = (id) => {
+        setTodos((prevState) =>
+            prevState.map((todo) => {
+                if (todo.id === id) {
+                    return {
+                        ...todo,
+                        completed: !todo.completed,
+                    };
+                }
+                return todo;
+            })
+        );
+    };
+
+    const delTodo = (id) => {
+        setTodos([...todos.filter((todo) => todo.id !== id)]);
+    };
+
+    const addTodoItem = (title, priority, category, dueDate) => {
+        const newTodo = {
+            id: uuidv4(),
+            title,
+            priority,
+            category,
+            dueDate,
+            completed: false,
+        };
+        setTodos([...todos, newTodo]);
+    };
+
+    const setUpdate = (updatedTitle, id) => {
+        setTodos(
+            todos.map((todo) => {
+                if (todo.id === id) {
+                    todo.title = updatedTitle;
+                }
+                return todo;
+            })
+        );
+    };
+
+    useEffect(() => {
+        const temp = JSON.stringify(todos);
+        localStorage.setItem("todos", temp);
+    }, [todos]);
+
+    return (
+        <div className={styles.inner}>
+            <Header />
+            <InputTodo addTodoProps={addTodoItem} />
+            <TodosList
+                todos={todos}
+                handleChangeProps={handleChange}
+                deleteTodoProps={delTodo}
+                setUpdate={setUpdate}
+            />
+        </div>
     );
-  };
-
-  // componentDidUpdate
-  useEffect(() => {
-    // storing todos items
-    const temp = JSON.stringify(todos);
-    localStorage.setItem("todos", temp);
-  }, [todos]);
-
-  return (
-    <div className={styles.inner}>
-      <Header />
-      <InputTodo addTodoProps={addTodoItem} />
-      <TodosList
-        todos={todos}
-        handleChangeProps={handleChange}
-        deleteTodoProps={delTodo}
-        setUpdate={setUpdate}
-      />
-    </div>
-  );
 };
 
 export default TodoContainer;
